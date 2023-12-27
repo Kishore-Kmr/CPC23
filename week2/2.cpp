8983a8cce910b7c428a3708e51d38ce3cb9fb84b
Kishore-Kmr <105747342+Kishore-Kmr@users.noreply.github.com>
2023-12-27 09:55:32 +0530

    Update 2.cpp

--- week2/2.cpp
+++ week2/2.cpp
@@ -1,1 +1,9 @@
-
+bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    unordered_set<int> st;
+	    for(int i=0;i<n;i++){
+	        if(st.find(x-arr[i])!=st.end())
+	            return true;
+            st.insert(arr[i]);
+	    }
+	    return false;
+}
