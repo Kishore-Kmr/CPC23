cf4ffcd7622227366c123e8b81e7b7493317581c
Kishore-Kmr <105747342+Kishore-Kmr@users.noreply.github.com>
2023-12-30 16:21:13 +0530

    Update 19.cpp

--- week2/19.cpp
+++ week2/19.cpp
@@ -1,16 +1,4 @@
-void transform(vector<vector<int>>& matrix, bool row, int &r, int &c){
-        int m=matrix.size(), n=matrix[0].size();
-        if(row){
-            for(int i=1;i<n;i++)
-                matrix[r][i]=1;
-        }
-        else{
-            for(int i=1;i<m;i++)
-                matrix[i][c]=1;
-        }
-        
-    }
-    void booleanMatrix(vector<vector<int> > &matrix)
+void booleanMatrix(vector<vector<int> > &matrix)
     {
         int m=matrix.size(), n=matrix[0].size();
         bool row=false, col=false;
@@ -28,20 +16,24 @@ void transform(vector<vector<int>>& matrix, bool row, int &r, int &c){
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(matrix[i][j])
-                    matrix[i][0]=1, matrix[0][j]=1;
+                if(matrix[i][j]){
+                    matrix[i][0]=1;
+                    matrix[0][j]=1;
+                }
             }
         }
-        for(int i=1;i<n;i++){
-            if(matrix[0][i])
-                transform(matrix, false, i, i);
-            matrix[0][i]=row;
-        }
         for(int i=1;i<m;i++){
-            if(matrix[i][0])
-                transform(matrix, true, i, i);
-            matrix[i][0]=col;
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]||matrix[i][0])
+                    matrix[i][j]=1;
+            }
         }
-        if(row||col)
-                matrix[0][0]=1;
+        if(row){
+            for(int i=0;i<n;i++)
+                matrix[0][i]=1;
+        }
+        if(col){
+            for(int i=0;i<m;i++)
+                matrix[i][0]=1;
+        }
     }
