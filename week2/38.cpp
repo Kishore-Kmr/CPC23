5b1058995452c0d51ad467c4bd8201dfb37399f3
Kishore-Kmr <105747342+Kishore-Kmr@users.noreply.github.com>
2024-01-01 10:34:18 +0530

    Create 38.cpp

---
+++ week2/38.cpp
@@ -0,0 +1,8 @@
+int maxProfit(vector<int>& prices) {
+        int profit=0, n=prices.size();
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1])
+                profit+=prices[i]-prices[i-1];
+        }
+        return profit;
+}
