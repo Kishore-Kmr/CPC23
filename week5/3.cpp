f000d7e5e8bfff14d741a523126480117736b640
Kishore-Kmr <105747342+Kishore-Kmr@users.noreply.github.com>
2024-01-17 19:45:02 +0530

    Create 3.cpp

---
+++ week5/3.cpp
@@ -0,0 +1,21 @@
+int search(int A[], int l, int h, int key){
+        int left=l, right=h;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(A[mid]==key)
+                return mid;
+            else if(A[mid]>=A[0]){
+                if(A[mid]<key||key<A[left])
+                    left=mid+1;
+                else
+                    right=mid-1;
+            }
+            else{
+                if(A[mid]>key||key>A[right])
+                    right=mid-1;
+                else 
+                    left=mid+1;
+            }
+        }
+        return -1;
+}
